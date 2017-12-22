3613fd5a4a44f13034188d907e1fcd99365adb18
ilyes kooli <kooli.ilyes@gmail.com>
2017-12-22 15:22:29 +0200

    Add bootstrap #6

--- symfony/Controller/DockerComposeController.php
+++ symfony/Controller/DockerComposeController.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\BuildSymfony;
+use App\Form\DockerComposeType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
@@ -33,10 +32,7 @@ class DockerComposeController extends AbstractController
      */
     public function index(Request $request): Response
     {
-        $form = $this->createFormBuilder()
-            ->add('php-version', ChoiceType::class, ['choices' => ['5.x' => '5-fpm', '7.x' => '7-fpm']])
-            ->add('submit', SubmitType::class)
-            ->getForm();
+        $form = $this->createForm(DockerComposeType::class);
 
         $form->handleRequest($request);
 
@@ -45,7 +41,6 @@ class DockerComposeController extends AbstractController
             $response = new Response($yaml);
             $dispositionHeader = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'docker-compose.yml');
             $response->headers->set('Content-Disposition', $dispositionHeader);
-
 
             return $response;
         }
--- symfony/Form/DockerComposeType.php
+++ symfony/Form/DockerComposeType.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class DockerComposeType extends AbstractType
 {
-
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('php-version', ChoiceType::class, ['choices' => ['5.x' => '5-fpm', '7.x' => '7-fpm']])
+            ->add('submit', SubmitType::class);
+    }
 }
